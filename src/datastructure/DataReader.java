0d53c0197c7ceb3d5b17ea373487cae803442e44
Boris1329 <boris.abunagimov@gmail.com>
2023-04-09 15:47:39 -0400

    Datastructure complete

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,5 +1,18 @@
 package datastructure;
 
+import algorithm.Sort;
+import databases.ConnectToSqlDB;
+
+import javax.sound.midi.Soundbank;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -19,6 +32,56 @@ public class DataReader {
 		 */
 
 		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\wormi\\IdeaProjects\\midterm-coding-exam\\src\\data\\self-driving-car"));
+			String textLine = in.readLine();
+			String locomotive = textLine;
+			String[] textWords;
+			while (textLine != null) {
+				System.out.println(textLine);
+				textLine = in.readLine();
+				locomotive += textLine;
+			}
+			locomotive = locomotive.replaceAll("\\.", "");
+			locomotive = locomotive.replaceAll("\\,", "");
+			textWords = locomotive.split(" ");
+			connectToSqlDB.insertDataFromStringArrayToSqlTable(textWords,"text_document" ,"words_from_file");
+			in.close();
+			List<String> allWordsList = new LinkedList<String>();
+			Stack<String> everyWordStack = new Stack<String>();
+			for (int i = 0; i < textWords.length; i++) {
+				allWordsList.add(textWords[i]);
+				everyWordStack.push(textWords[i]);
+			}
+			Iterator iterator = allWordsList.iterator();
+			locomotive = (String) iterator.next();
+			while (iterator.hasNext()) {
+				System.out.println(locomotive);
+				locomotive = (String) iterator.next();
+			}
+			System.out.println(locomotive);
+			Iterator iterator2 = everyWordStack.iterator();
+			while (iterator2.hasNext()) {
+				System.out.println(everyWordStack.pop());
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println(ex);
+		} catch (IOException ex) {
+			System.out.println(ex);
+		}
+		Stack<String> geronimo = new Stack<String>();
+		geronimo.push("Item 1");
+		geronimo.push("Item 2");
+		geronimo.push("Item 3");
+		geronimo.push("Item 4");
+		geronimo.push("Item 5");
+		geronimo.push("Item 6");
+
+		System.out.println("The peek is " + geronimo.peek());
+		System.out.println("The first element to escape is " + geronimo.pop());
+		System.out.println("I want to find item 4 (" + geronimo.search("Item 4") + ") <- this number" +
+				"tells me how far it is from the exit point");
 
 
 
--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import databases.ConnectToSqlDB;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,6 +14,37 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
+
+		ArrayList<String> volcanoes = new ArrayList<String>();
+		volcanoes.add("Krakatoa");
+		volcanoes.add("Vesuvius");
+		volcanoes.add("Mauna Loa");
+		volcanoes.add("Nevado del Ruiz");
+		volcanoes.add("Mount Pinatubo");
+		volcanoes.add("Donuts");
+
+		Iterator iterator = volcanoes.iterator();
+
+		while (iterator.hasNext()) {
+			Object volcano = iterator.next();
+			System.out.println(volcano + " ");
+		}
+		System.out.println("But donuts are not volcano");
+		volcanoes.remove(volcanoes.size()-1);
+
+		for (int i = 0; i <volcanoes.size(); i++) {
+			System.out.println(volcanoes.get(i));
+		}
+
+		System.out.println("The peek of the arrayLits is " + volcanoes.get(0));
+
+
+		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+
+		for(int i = 0; i<volcanoes.size(); i++){
+			String inItGoes = volcanoes.get(i);
+			connectToSqlDB.insertDataFromStringToSqlTable(inItGoes, "volcanos", "volcanoes_names");
+		}
 	
 
 	}
