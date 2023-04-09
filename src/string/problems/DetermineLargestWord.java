87198cc5f5201473550ffafe01d044a4c2bdf3b9
Boris1329 <boris.abunagimov@gmail.com>
2023-04-09 15:27:45 -0400

    String-problems complete

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -1,23 +1,47 @@
 package string.problems;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class DetermineLargestWord {
+    Map<Integer,String> expectedMap = new LinkedHashMap<Integer, String>();
+
+    String s="Human brain is a biological learning machine";
+    Scanner scanner = new Scanner(s);
+    String test =scanner.next();
+
     public static void main(String[] args) {
+        DetermineLargestWord determineLargestWord = new DetermineLargestWord();
+        System.out.println(determineLargestWord.largestWord(determineLargestWord.s));
+
         /*
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
-        String s="Human brain is a biological learning machine";
-        Map<Integer, String> wordNLength = findTheLargestWord(s);
+    }
+    public LinkedHashMap<Integer, String> largestWord(String thisOne) {
+        scanner = new Scanner(thisOne);
+        while (scanner.hasNext()) {
+            String another = scanner.next();
+            if (another.length() > test.length()) {
+                test = another;
+            }
+        }
+
+        LinkedHashMap<Integer, String> wordNLength = findTheLargestWord(test);
+
         //implement
+        return wordNLength;
 
     }
 
-    public static Map<Integer, String> findTheLargestWord(String wordGiven){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        String st = "";
+    public static LinkedHashMap<Integer, String> findTheLargestWord(String wordGiven){
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<Integer, String>();
+        map.put(wordGiven.length(),wordGiven);
         //implement
 
         return map;
--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -1,8 +1,50 @@
 package string.problems;
 
+import org.junit.Assert;
+
+
 public class UnitTestingStringProblem {
-    public static void main(String[] args) {
-        //Apply Unit Test into all the methods in this package.
+    Anagram anagram = new Anagram();
+    DetermineLargestWord determineLargestWord = new DetermineLargestWord();
+    DuplicateWord duplicateWord = new DuplicateWord();
+    Palindrome palindrome = new Palindrome();
+    Permutation permutation = new Permutation();
+
+
+    //Apply Unit Test into all the methods in this package.
+
+
+    public void anagram() {
+        Assert.assertTrue(anagram.isAnagram(anagram.cat,anagram.act ));
+        System.out.println("Anagram test passed");
+    }
+
+    public void largerWord() {
+        determineLargestWord.expectedMap.put(10,"biological");
+        Assert.assertEquals(determineLargestWord.expectedMap,determineLargestWord.largestWord(determineLargestWord.s));
+        System.out.println("Determine largest word test passed");
+    }
+
+    public void duplicateWord() {
+        duplicateWord.expectedMap.put(duplicateWord.javaCount,duplicateWord.java);
+        duplicateWord.expectedMap.put(duplicateWord.isCount,duplicateWord.is);
+        duplicateWord.expectedMap.put(duplicateWord.languageCount,duplicateWord.language);
+        Assert.assertEquals(duplicateWord.duplicateWord(duplicateWord.st),duplicateWord.expectedMap);
+        System.out.println("Duplicate word test passed");
+    }
+
+    public void palindrome() {
+        Assert.assertTrue(palindrome.palindrome(palindrome.theOneToUse));
+        System.out.println("Palindrome test passed");
+    }
+
+    public void permutation() {
+        permutation.permutation("ABC");
+        Assert.assertEquals(permutation.expectedOutput,permutation.actualOutput);
+        System.out.println("Permutation test passed");
 
     }
+
+
+
 }
